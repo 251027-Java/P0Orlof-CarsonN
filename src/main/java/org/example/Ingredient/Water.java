ca326e46cd64021a465b34129f76f6e8c8860fae
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2025-11-12 16:02:02 -0600

    Instantiated Common Ingredients

---
+++ src/main/java/org/example/Ingredient/Water.java
@@ -0,0 +1,33 @@
+package org.example.Ingredient;
+
+public class Water implements Ingredient{
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double getCups() {
+        return 0;
+    }
+
+    @Override
+    public double getGrams() {
+        return 0;
+    }
+
+    @Override
+    public double convertGramsToCups(double grams) {
+        return 0;
+    }
+
+    @Override
+    public double convertCupsToGrams(double cups) {
+        return 0;
+    }
+    //Fields
+
+    //Constructor
+
+    //Methods
+}
